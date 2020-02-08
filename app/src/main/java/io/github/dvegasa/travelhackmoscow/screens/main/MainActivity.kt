e02981bd48786201c80f383edef916fe9d8b07f3
Ed <--->
2020-02-08 17:01:34 +0300

    Чё пацаны, новый боттомнав?
    Took 34 minutes

--- app/src/main/java/io/github/dvegasa/travelhackmoscow/screens/main/MainActivity.kt
+++ app/src/main/java/io/github/dvegasa/travelhackmoscow/screens/main/MainActivity.kt
@@ -1,44 +1,64 @@
 package io.github.dvegasa.travelhackmoscow.screens.main
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import io.github.dvegasa.travelhackmoscow.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var currentScreen = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initBottomNav()
+        updateUi()
+    }
+
+    private fun updateUi() {
+        val bnavs = listOf(bnavDiscover, bnavGroups, bnavMap, bnavHelp)
+        bnavs.forEach {
+            DrawableCompat.setTint(
+                DrawableCompat.wrap(it.drawable),
+                ResourcesCompat.getColor(resources, R.color.colorBottomnavInactive, null)
+            )
+        }
+
+        val active = when (currentScreen) {
+            0 -> bnavDiscover
+            1 -> bnavGroups
+            2 -> bnavMap
+            else -> bnavHelp
+        }
+        DrawableCompat.setTint(
+            DrawableCompat.wrap(active.drawable),
+            ResourcesCompat.getColor(resources, R.color.colorAccent, null)
+        )
     }
 
     private fun initBottomNav() {
-        val item1 = AHBottomNavigationItem("", R.drawable.ic_discover)
-        val item2 = AHBottomNavigationItem("", R.drawable.ic_group)
-        val item3 = AHBottomNavigationItem("", R.drawable.ic_map)
-        val item4 = AHBottomNavigationItem("", R.drawable.ic_help)
+        bnavDiscover.setOnClickListener {
+            currentScreen = 0
+            updateUi()
+        }
 
-        bottomnav.addItem(item1)
-        bottomnav.addItem(item2)
-        bottomnav.addItem(item3)
-        bottomnav.addItem(item4)
+        bnavGroups.setOnClickListener {
+            currentScreen = 1
+            updateUi()
+        }
 
-        bottomnav.defaultBackgroundColor = Color.parseColor("#FFFFFF")
+        bnavMap.setOnClickListener {
+            currentScreen = 2
+            updateUi()
+        }
 
-        bottomnav.accentColor = Color.parseColor("#F2694F") // accent
-
-        bottomnav.isColored = false
-
-        bottomnav.titleState = AHBottomNavigation.TitleState.ALWAYS_HIDE
-
-        bottomnav.setOnTabSelectedListener { pos, wasSelected ->
-
-            true
+        bnavHelp.setOnClickListener {
+            currentScreen = 3
+            updateUi()
         }
     }
 }
