30b7fe81bed657a490ff97da0778365ed0252970
Ed <--->
2020-02-08 17:39:16 +0300

    Статус барина теперь как нужно. Начинаю верстать мейн скрин
    Took 30 minutes

--- app/src/main/java/io/github/dvegasa/travelhackmoscow/screens/main/MainActivity.kt
+++ app/src/main/java/io/github/dvegasa/travelhackmoscow/screens/main/MainActivity.kt
@@ -14,7 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // fullscreen()
+        supportActionBar?.hide()
         initBottomNav()
         updateUi()
     }
